7359ae760fd6a5980be8f5a7218b935555510ba8
Tamim Arafat <31634638+arafatamim@users.noreply.github.com>
2025-04-16 11:22:38 +0600

    chore: remove Vercel-specific dependencies and configurations

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,9 +1,6 @@
-import { vercelPreset } from "@vercel/react-router/vite";
 import type { Config } from "@react-router/dev/config";
 
 export default {
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-  prerender: ["/profile", "/projects", "/contact"],
-  presets: [vercelPreset()],
 } satisfies Config;
